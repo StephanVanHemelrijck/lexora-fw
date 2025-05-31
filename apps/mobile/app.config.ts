15baf23e9d273d0d705077fa59ff9e454037d583
Stephan Van Hemelrijck <stephan.van.hemelrijck@student.ehb.be>
2025-05-31 15:33:10 +0200

    Update app.config.ts

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -19,6 +19,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       foregroundImage: './assets/images/adaptive-icon.png',
       backgroundColor: '#ffffff',
     },
+    package: 'com.anonymous.mobile',
   },
   web: {
     bundler: 'metro',
