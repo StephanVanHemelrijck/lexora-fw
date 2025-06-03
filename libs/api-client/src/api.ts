14cdac1a3ab1177e0faf0ca8b9176fd8179ac971
Stephan Van Hemelrijck <stephan.van.hemelrijck@student.ehb.be>
2025-06-03 20:31:59 +0200

    Update api.ts

--- libs/api-client/src/api.ts
+++ libs/api-client/src/api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const baseURL = 'http://192.168.0.129:3000/api';
-// const baseURLProd = 'https://stephanvh.com/api';
+// const baseURL = 'http://192.168.0.129:3000/api';
+const baseURLProd = 'https://stephanvh.com/api';
 
 export const api = axios.create({
-  baseURL: baseURL,
+  baseURL: baseURLProd,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
