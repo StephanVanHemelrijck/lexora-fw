97ff8c048d7be33e15fb74666f83618428275869
Stephan Van Hemelrijck <stephan.van.hemelrijck@student.ehb.be>
2025-06-13 13:02:31 +0200

    Update app.config.ts

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -18,7 +18,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   android: {
     adaptiveIcon: {
-      foregroundImage: './assets/images/lex_cropped.png',
+      foregroundImage: './assets/images/lex.png',
       backgroundColor: '#242933',
     },
     package: 'com.anonymous.mobile',
